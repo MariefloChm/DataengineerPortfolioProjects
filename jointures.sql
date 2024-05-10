7f820aae27ee22545a9e55742641ad6b7a69549a
MariefloChm <123181986+MariefloChm@users.noreply.github.com>
2024-05-10 12:02:39 +0200

    Update jointures.sql

--- jointures.sql
+++ jointures.sql
@@ -1,7 +1,38 @@
--- Joindre la table des utilisateurs et la table des sessions pour obtenir une vue complète
-SELECT u.User_ID, u.Age, u.Gender, s.Session_ID, s.Video_ID, s.Duration_Watched
-FROM Users u
-JOIN Streaming s ON u.User_ID = s.User_ID;
+-- Jointure pour montrer les genres les plus populaires par pays
+SELECT 
+    L.Country, 
+    S.Genre, 
+    COUNT(*) AS Count_Views
+FROM Streaming S
+INNER JOIN Users U ON S.User_ID = U.User_ID
+INNER JOIN Location L ON U.Location_ID = L.Location_ID
+GROUP BY L.Country, S.Genre
+ORDER BY L.Country, COUNT(*) DESC;
+
+-- Jointure pour montrer les top villes pour chaque genre de vidéo
+SELECT 
+    L.City, 
+    S.Genre, 
+    COUNT(*) AS Count_Views
+FROM Streaming S
+INNER JOIN Users U ON S.User_ID = U.User_ID
+INNER JOIN Location L ON U.Location_ID = L.Location_ID
+GROUP BY L.City, S.Genre
+ORDER BY S.Genre, COUNT(*) DESC;
+
+-- Jointure pour montrer les top utilisateurs dans les principales villes
+SELECT 
+    U.User_ID,
+    U.First_Name,
+    U.Last_Name,
+    L.City,
+    COUNT(*) AS Total_Watches
+FROM Streaming S
+INNER JOIN Users U ON S.User_ID = U.User_ID
+INNER JOIN Location L ON U.Location_ID = L.Location_ID
+WHERE L.City IN ('New York', 'Los Angeles', 'Paris', 'London', 'Tokyo')
+GROUP BY U.User_ID, U.First_Name, U.Last_Name, L.City
+ORDER BY COUNT(*) DESC;
 
 -- Analyse des préférences de genre par sexe
 SELECT u.Gender, s.Genre, COUNT(*) AS Total_Watches
